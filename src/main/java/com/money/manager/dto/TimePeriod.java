dd2f5aec022c9a0eb9ec4a3863dbe6987239be8c
Martyna <trejsu@gmail.com>
2017-12-16 17:25:55 +0100

    Fixes min and max date in TimePeriod

--- src/main/java/com/money/manager/dto/TimePeriod.java
+++ src/main/java/com/money/manager/dto/TimePeriod.java
@@ -13,14 +13,22 @@ import static org.springframework.util.StringUtils.isEmpty;
 @StartDateBeforeEndDate
 public class TimePeriod {
 
-    private final static String MIN_DATE = "0000-00-00";
-    private final static String MAX_DATE = "9999-99-99";
+    private final static String MIN_DATE = "0000-01-01";
+    private final static String MAX_DATE = "9999-12-31";
 
     private String start;
     private String end;
 
     public TimePeriod(String start, String end) {
         this.start = isEmpty(start) ? MIN_DATE : start;
+        this.end = isEmpty(end) ? MAX_DATE : end;
+    }
+
+    public void setStart(String start) {
+        this.start = isEmpty(start) ? MIN_DATE : start;
+    }
+
+    public void setEnd(String end) {
         this.end = isEmpty(end) ? MAX_DATE : end;
     }
 }
