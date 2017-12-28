baf12e39bfd30f6719916b0028a4e4151de1db09
Martyna <trejsu@gmail.com>
2017-12-28 15:46:32 +0100

    Fixes bug with 500 after logout before login

--- src/main/java/com/money/manager/auth/login/LogoutServlet.java
+++ src/main/java/com/money/manager/auth/login/LogoutServlet.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import static java.util.Optional.ofNullable;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/logout")
 public class LogoutServlet {
+
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
-        session.invalidate();
+        ofNullable(session).ifPresent(HttpSession::invalidate);
         return ResponseEntity.ok().build();
     }
 }
