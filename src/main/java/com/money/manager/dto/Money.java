e1daa615cc3a6cd0f1cc0f459de926a05c7a06a5
Martyna <trejsu@gmail.com>
2018-01-03 16:03:49 +0100

    Adds creation of Money instance with currency code with PLN default

--- src/main/java/com/money/manager/dto/Money.java
+++ src/main/java/com/money/manager/dto/Money.java
@@ -1,14 +1,26 @@
 package com.money.manager.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import static java.util.Optional.ofNullable;
+
 @Value
 public class Money {
 
     private BigDecimal amount;
+
+    @JsonIgnore
     private Currency currency;
 
+    @JsonCreator
+    public Money(@JsonProperty("amount") BigDecimal amount, @JsonProperty("currency") String currencyCode) {
+        this.amount = amount;
+        this.currency = Currency.getInstance(ofNullable(currencyCode).orElse("PLN"));
+    }
 }
